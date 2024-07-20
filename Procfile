b9e708be320e0589dd1b1119b06ba2cc21599f20
JerickTwO <Jerickjjtm774@gmail.com>
2024-07-20 17:11:14 -0500

    whitenoise

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn dealer_project.wsgi
+web: python manage.py collectstatic && gunicorn dealer_project.wsgi
--- dealer_project/settings.py
+++ dealer_project/settings.py
@@ -23,11 +23,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'dealer_app',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -108,5 +110,10 @@ STATIC_FILE_DIRS = 'dealer_app/static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['localhost', 'cisne-car.up.railway.app']
+
+# ALLOWED_HOSTS = ['localhost', 'cisne-car.up.railway.app']
